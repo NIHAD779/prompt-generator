66031ed59b8f8de7d0a022591cb9ae7befe380bf
nihad.u <nihad.u@digitral.com>
2026-01-13 16:19:16 +0530

    update manifest with new icon

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,25 +12,25 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: 'portrait-primary',
     icons: [
       {
-        src: '/icon-192.png',
+        src: '/icon.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable',
       },
       {
-        src: '/icon-512.png',
+        src: '/icon.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
       },
       {
-        src: '/icon-192.png',
+        src: '/icon.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'any',
       },
       {
-        src: '/icon-512.png',
+        src: '/icon.png',
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any',
